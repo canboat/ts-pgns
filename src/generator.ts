36d6ec0105dad38d899e9402e28e267e455ceeb6
Scott Bender <scott@scottbender.net>
2025-07-14 20:32:16 -0400

    fix: make enum values real enums (#21)

--- src/generator.ts
+++ src/generator.ts
@@ -78,25 +78,24 @@ if (argv.enums) {
 
     done = {}
     category()
-    console.log(
-      `export const ${enumName(en.Name)}Values : {[key: string]: number} = {`
-    )
+    console.log(`export enum ${enumName(en.Name)}Values {`)
     en.EnumValues.forEach((v: any) => {
       const name = enumName(v.Name)
       const found = done[name]
       if (found) {
         const num = done[name] + 1
         done[name] = num
-        console.log(`  ${name}${num}: 0x${v.Value.toString(16)},`)
+        console.log(`  ${name}${num} = 0x${v.Value.toString(16)},`)
       } else {
         done[name] = 1
-        console.log(
-          `  [${enumName(en.Name)}.${name}]: 0x${v.Value.toString(16)},`
-        )
+        console.log(`  ${name} = 0x${v.Value.toString(16)},`)
       }
     })
     if (en.Name === 'INDUSTRY_CODE') {
-      console.log(`  Marine: 0x4,`)
+      console.log(
+        '  // eslint-disable-next-line @typescript-eslint/no-duplicate-enum-values'
+      )
+      console.log(`  Marine = 0x4,`)
     }
     console.log('}\n')
   })
